9c1f178f4225cb6dacfe1d3399e3e4facee42f8f
EddMal <107246914+EddMal@users.noreply.github.com>
2024-11-04 12:46:19 +0100

    Updated Footer and header. Users are now able to add links for header and footer. 1.DoubleInputForm changed: Description/instructions for links, input changed to textarea for TextInput2.
    2.DoubleInputForm.razxor.css changed:
    minor css updates.
    
    3. FooterTemplate.razor changed:
    The templet can now interpret links.
    
    4. FooterTemplate..razor added:
    contains updated style for footer.
    
    5. HeaderTemplate.razor changed:
    The templet can now interpret links.
    
    6. HeaderTemplate.razor added:
    contains updated style for header.
    
    7.InputProcessor.Cs added:
    Used to process links.
    
    8.PartDetail.cs added.
    Class contains data for the processing, used of methoods in InputProcessor.razor

---
+++ CMS/Shared/InputProcessor.cs
@@ -0,0 +1,56 @@
+﻿using CMS.Shared;
+
+public class InputProcessor
+{
+    public static PartDetail[] SplitInput(string input,string delimiter)
+    {
+        var parts = SplitInputIntoParts(input, delimiter);
+        var partDetails = new List<PartDetail>();
+
+        foreach (var part in parts)
+        {
+            var detail = ProcessPart(part);
+            if (detail != null)
+            {
+                partDetails.Add(detail);
+            }
+        }
+
+        return partDetails.ToArray();
+    }
+
+    private static string[] SplitInputIntoParts(string input, string delimiter)
+    {
+        return input.Split(new string[] { delimiter }, StringSplitOptions.None);
+    }
+
+    private static PartDetail ProcessPart(string part)
+    {
+        string linkDelimiter = "=";
+        string[] partAndUrl = part.Split(new string[] { linkDelimiter }, StringSplitOptions.None);
+
+        if (partAndUrl.Length >= 2 && !string.IsNullOrEmpty(partAndUrl[0]) && !string.IsNullOrEmpty(partAndUrl[1]))
+        {
+            // It's a link
+            return new PartDetail
+            {
+                IsLink = true,
+                DisplayText = partAndUrl[0],
+                Url = partAndUrl[1]
+            };
+        }
+        else if (partAndUrl.Length >= 1 && !string.IsNullOrEmpty(partAndUrl[0]))
+        {
+            // It's plain text
+            return new PartDetail
+            {
+                IsLink = false,
+                DisplayText = partAndUrl[0],
+                Url = null
+            };
+        }
+
+        // Return null if no valid part was found
+        return null;
+    }
+}
